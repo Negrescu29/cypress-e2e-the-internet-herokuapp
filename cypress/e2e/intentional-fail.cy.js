c7f6b1ce25ee5ce3e50bba9ed9d5b4d66d78f83f
Ion Negrescu <Negrescu29@users.noreply.github.com>
2025-06-21 18:54:43 +0200

    skipping failing test

--- cypress/e2e/intentional-fail.cy.js
+++ cypress/e2e/intentional-fail.cy.js
@@ -1,5 +1,5 @@
 describe("Intentional Failure Demo", () => {
-  it("should fail on purpose to trigger screenshot and video capture", () => {
+  it.skip("should fail on purpose to trigger screenshot and video capture", () => {
     cy.visit("https://the-internet.herokuapp.com/");
     // This selector does not exist, so the test will fail
     cy.get(".this-element-does-not-exist").should("be.visible");
